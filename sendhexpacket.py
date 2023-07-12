9ffd1884170c3721c20cf0d87ddea6493b399dbf
xenjin450 <136635395+xenjin450@users.noreply.github.com>
2023-07-12 19:56:28 +0700

    Add files via upload

---
+++ sendhexpacket.py
@@ -0,0 +1,16 @@
+import socket 
+import sys 
+
+#Target IP/Port
+HOST = input("Target Host/IP:")
+PORT = int(input("Target Port:"))
+#hexdata to send 
+hexdata = ''
+#Encoding Hex Data To Bytes 
+hexpacket = bytes.fromhex(hexdata)
+#Socket Configuration
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Connect To Traget IP/Port
+s.connect((HOST,PORT))
+#Send The Final Hex Packet
+s.send(hexpacket)
